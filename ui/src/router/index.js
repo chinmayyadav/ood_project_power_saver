ac589c4e941af8eefb991bb02322e9c5fa4414fa
Chinmay Yadav <chinmayyadav98@gmail.com>
2023-04-07 19:27:01 -0400

    Initial Commit

---
+++ ui/src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '../views/HomePage.vue'
+// import LoginView from '../components/Login.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomePage
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../layouts/DashboardLayout.vue')
+
+  },
+  // {
+  //   path: '/login',
+  //   name: 'login',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "Login" */ '../componenets/Login.vue')
+  // }
+]
+
+const router = new VueRouter({
+  mode: process.env.IS_ELECTRON ? 'hash' : 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ ui/src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <!-- <h3>Welcome to PowerSaver HQ</h3> -->
+    <login></login>
+  </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue'
+export default {
+  components: { Login },
+name: "HomePage"
+}
+</script>
+
+<style>
+
+</style>
