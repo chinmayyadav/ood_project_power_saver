fb4eddb673c2f9c2def358f459d766fbabc770b3
amanvelani <amanvelani@gmail.com>
2023-04-20 00:48:27 -0400

    Testing new branch

--- backend/src/main/java/com/syracuse/PowerSaverHQ/controllers/UserController.java
+++ backend/src/main/java/com/syracuse/PowerSaverHQ/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.syracuse.PowerSaverHQ.controllers;
 
 import com.syracuse.PowerSaverHQ.models.UserDetails;
+import com.syracuse.PowerSaverHQ.services.UserService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @RequestMapping(value = "/register-user",headers = "Accept=application/json", method = RequestMethod.POST)
     public String customerInformation(@RequestBody UserDetails user) {
+        UserService userDetails = new UserService();
+        System.out.println(userDetails.add());
 
-        return "Customer information saved successfully ::." + user.getFirstName() + " " + user.getLastName() + " " + user.getEmail();
     }
 }
--- backend/src/main/java/com/syracuse/PowerSaverHQ/services/UserService.java
+++ backend/src/main/java/com/syracuse/PowerSaverHQ/services/UserService.java
@@ -1,4 +1,14 @@
 package com.syracuse.PowerSaverHQ.services;
 
+import org.hibernate.sql.Insert;
+
 public class UserService {
+    public boolean jdbcConnection(){
+        return true;
+    }
+    public boolean add(){
+        jdbcConnection();
+        Insert
+        return true;
+    }
 }
