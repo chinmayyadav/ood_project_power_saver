24b79231d5d21cb7309cfa7648d3fb925221c7c5
amanvelani <amanvelani@gmail.com>
2023-04-20 19:22:51 -0400

    Merging Conflicts

--- backend/src/main/java/com/syracuse/PowerSaverHQ/services/UserService.java
+++ backend/src/main/java/com/syracuse/PowerSaverHQ/services/UserService.java
@@ -1,14 +1,47 @@
 package com.syracuse.PowerSaverHQ.services;
 
-import org.hibernate.sql.Insert;
 
-public class UserService {
-    public boolean jdbcConnection(){
-        return true;
-    }
-    public boolean add(){
-        jdbcConnection();
-        Insert
-        return true;
+import com.syracuse.PowerSaverHQ.models.UserDetails;
+import com.syracuse.PowerSaverHQ.utils.databaseConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+import static com.syracuse.PowerSaverHQ.utils.generateAccountNumber.generateAccountNumber;
+
+@Service
+public class UserService extends databaseConnection {
+    public Boolean RegisterUser(UserDetails user) {
+
+
+            try {
+            String AccountNumber = generateAccountNumber();
+            String FirstName = user.getFirstName();
+            String LastName = user.getLastName();
+            String Email = user.getEmail();
+            String Password = user.getPassword();
+            String query = "insert into UserDetails (FirstName, LastName, Email, Password, AccountNumber )  values  (?,?,?,?,?)";
+
+            PreparedStatement stmt = sql_connection.prepareStatement(query);
+
+            stmt.setString(1, FirstName);
+            stmt.setString(2, LastName);
+            stmt.setString(3, Email);
+            stmt.setString(4, Password);
+            stmt.setString(5, AccountNumber);
+            int row = stmt.executeUpdate();
+            return true;
+        }
+
+        catch (SQLException SE){
+                    System.out.println("SQL SERVER ERROR");
+                    System.out.println(SE.getMessage());
+                    return false;
+        }
+
     }
 }
