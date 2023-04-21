afa163086ce7d63f4fa2d26ce6b4b638e02332cf
amanvelani <amanvelani@gmail.com>
2023-04-20 20:50:31 -0400

    Adresses Module Added

--- backend/src/main/java/com/syracuse/PowerSaverHQ/services/AppliancesService.java
+++ backend/src/main/java/com/syracuse/PowerSaverHQ/services/AppliancesService.java
@@ -1,23 +1,23 @@
 package com.syracuse.PowerSaverHQ.services;
 
 import com.syracuse.PowerSaverHQ.models.ApplianceDetails;
+import com.syracuse.PowerSaverHQ.models.UserDetails;
 import com.syracuse.PowerSaverHQ.utils.databaseConnection;
 import org.json.JSONArray;
 import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+
 import org.json.JSONObject;
 
 @Service
 public class AppliancesService extends databaseConnection {
-    public JSONArray getApplianceData(float rating){
+    public JSONArray getApplianceData(ApplianceDetails applianceDetails){
         try{
             String query = "SELECT * FROM HomeAppliances WHERE ApplianceRating = ?";
             PreparedStatement pstmt = sql_connection.prepareStatement(query);
-            pstmt.setFloat(1, rating);
+            pstmt.setFloat(1, applianceDetails.getRating());
             ResultSet rs = pstmt.executeQuery();
             JSONArray jsonArray = new JSONArray();
             while(rs.next()){
@@ -42,7 +42,7 @@ public class AppliancesService extends databaseConnection {
             int addID = applianceDetails.getAddID();
             int applianceID = applianceDetails.getApplianceID();
             int count = applianceDetails.getCount();
-            float hours = applianceDetails.getHours();
+            int hours = applianceDetails.getHours();
             int days = applianceDetails.getDays();
 
             String query = "insert into AddressApplianceMapping (AddressID, ApplianceID, Count, NoOfHours, NoOfDays )  values  (?,?,?,?,?)";
@@ -51,7 +51,7 @@ public class AppliancesService extends databaseConnection {
             stmt.setInt(1,addID);
             stmt.setInt(2,applianceID);
             stmt.setInt(3,count);
-            stmt.setFloat(4,hours);
+            stmt.setInt(4,hours);
             stmt.setInt(5,days);
 
             stmt.executeUpdate();
@@ -64,14 +64,14 @@ public class AppliancesService extends databaseConnection {
         return false;
     }
 
-    public JSONArray getUserApplianceData(int id) {
+    public JSONArray getUserApplianceData(UserDetails userDetails) {
         try{
             String query = "SELECT A.AddressID, H.ApplianceName, A.AddressID, A.NoOfHours, A.NoOfDays FROM AddressApplianceMapping AS A\n" +
                     "JOIN UserAddress ON A.AddressID = UserAddress.ID\n" +
                     "JOIN HomeAppliances AS H ON H.ID = A.ApplianceID\n" +
                     "WHERE UserAddress.UserID = ?";
             PreparedStatement pstmt = sql_connection.prepareStatement(query);
-            pstmt.setFloat(1, id);
+            pstmt.setFloat(1, userDetails.getUserID());
             ResultSet rs = pstmt.executeQuery();
             JSONArray jsonArray = new JSONArray();
             while(rs.next()){
